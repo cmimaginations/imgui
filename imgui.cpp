a06ce2389e26e68d63bfdb2c59df0efe145c3618
ocornut <omar@miracleworld.net>
2025-03-31 21:43:15 +0200

    Fonts: GC Compact All exposed in Metrics->Memory Allocations includes compacting texture data.

--- imgui.cpp
+++ imgui.cpp
@@ -4475,6 +4475,7 @@ void ImGui::GcCompactTransientMiscBuffers()
     g.MultiSelectTempDataStacked = 0;
     g.MultiSelectTempData.clear_destruct();
     TableGcCompactSettings();
+    g.IO.Fonts->CompactCache();
 }
 
 // Free up/compact internal window buffers, we can use this when a window becomes unused.
